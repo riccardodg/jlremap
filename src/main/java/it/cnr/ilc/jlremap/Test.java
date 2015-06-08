901ff80ef84654fe93a9a2e99d64e84598891e53
riccardodg <riccardo.delgratta@ilc.cnr.it>
2015-06-08 15:40:37 +0200

    init jlremap

---
+++ src/main/java/it/cnr/ilc/jlremap/Test.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.cnr.ilc.jlremap;
+
+/**
+ *
+ * @author riccardo
+ */
+public class Test {
+    static void main(String[] args){
+        System.out.println("test ok");
+    }
+    
+}
